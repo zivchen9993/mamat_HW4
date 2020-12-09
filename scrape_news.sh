bfa1cdcfad7d1659b33911189bf34043ac412680
zivchen9993 <ziv.chen@campus.technion.ac.il>
2020-12-09 16:04:19 +0200

    updated results and more efficient scrape_news

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-#download the site HTML file
-wget https://www.ynetnews.com/category/3082
+#download the site HTML file, b = base address, l = link beggining
+b=$(wget https://www.ynetnews.com/category/3082 -q -O -) 
+l=https://www.ynetnews.com/article/
+results=""
 
-#search the file for relevant sites and save them on a temp file
-cat 3082 | grep -o -P 'https://www.ynetnews.com/article/.{0,9}["#]' >res_p.csv 
-
-#remove the last character (" or #) and remove any duplicated lines
-uniq res_p.csv | sed 's/.$//' > results_pre.csv 
-
-#loop, running through all the addresses and counting the appearences of names.
-#eventualy adds the names and amount to each address to the file.
+#making b (var) the correct list: search the file for l (var)
+#and the right suffix, remove the last character (" or #) and duplicated lines
+#loop, running through all the addresses in b (var) 
+#counting the appearences of names in each address.
+#eventualy adds the names and amount to each address in the variable.
 while IFS= read -r line; do
 	article=$(wget $line -q -O -)
 	N=`echo $article|grep -o "Netanyahu" | wc -l `
 	G=`echo $article|grep -o "Gantz"| wc -l`
+	results+="\n"
 	if (($N==0 && $G==0));then
-		echo $line, "-">> results.csv
+		results+=`echo $line, "-"`
 	else
-		echo $line, "Netanyahu", $N, "Gantz", $G>> results.csv
+		results+=`echo $line, "Netanyahu", $N, "Gantz", $G`
 	fi
-done<results_pre.csv
+done <<< `echo $b|grep -o -P "$l.{0,9}[\"#]" | uniq | sed 's/.$//'` 
 
-#adding the amount of articles to the beggining of the list.
+#puting the var data into the desired file
+echo -e $results > results.csv
+
+#adding the amount of articles to the beggining of the list 
+#(decreasing the first line).
 x=`wc -l < results.csv`
-sed -i "1i $x" results.csv
+wc -l < results.csv | sed -i "1c$((--x))" results.csv
