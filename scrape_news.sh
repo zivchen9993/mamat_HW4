2fc1ab85e6e3a3f33e4a41e99a8c2e4edbce66c6
zivchen9993 <ziv.chen@campus.technion.ac.il>
2020-12-10 11:39:10 +0200

    update according to conventions

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#download the site HTML file, b = base address, l = link beggining
-b=$(wget https://www.ynetnews.com/category/3082 -q -O -) 
-l=https://www.ynetnews.com/article/
+#download the site HTML file
+base=$(wget https://www.ynetnews.com/category/3082 -q -O -) 
+link=https://www.ynetnews.com/article/
 results=""
-
+net="Netanyahu"
+gan="Gantz"
 #making b (var) the correct list: search the file for l (var)
 #and the right suffix, remove the last character (" or #) and duplicated lines
 #loop, running through all the addresses in b (var) 
@@ -12,20 +13,20 @@ results=""
 #eventualy adds the names and amount to each address in the variable.
 while IFS= read -r line; do
 	article=$(wget $line -q -O -)
-	N=`echo $article|grep -o "Netanyahu" | wc -l `
-	G=`echo $article|grep -o "Gantz"| wc -l`
+	n=`echo $article|grep -o "$net" | wc -l `
+	g=`echo $article|grep -o "$gan"| wc -l`
 	results+="\n"
-	if (($N==0 && $G==0));then
+	if (($n==0 && $g==0));then
 		results+=`echo $line, "-"`
 	else
-		results+=`echo $line, "Netanyahu", $N, "Gantz", $G`
+		results+=`echo $line, "$net", $n, "$gan", $g`
 	fi
-done <<< `echo $b|grep -o -P "$l.{0,9}[\"#]" | uniq | sed 's/.$//'` 
+done <<< `echo $base|grep -o -P "$link.{0,9}[\"#]" | uniq | sed 's/.$//'` 
 
 #puting the var data into the desired file
 echo -e $results > results.csv
 
 #adding the amount of articles to the beggining of the list 
 #(decreasing the first line).
-x=`wc -l < results.csv`
-wc -l < results.csv | sed -i "1c$((--x))" results.csv
+temp=`wc -l < results.csv`
+wc -l < results.csv | sed -i "1c$((--temp))" results.csv
